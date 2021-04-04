a18366044a4580695be847c373c355491ce57c37
Anaam K <anam.DevDes27@gmail.com>
2021-04-04 15:31:08 +0530

    Added message to show that table is horizontally scrollable

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,11 @@ const TagLine = styled.h3`
   opacity: ${(props) => (props.logoAnimDone ? 1 : 0)};
 
   transition: opacity 0.3s ease 0.3s;
+
+  span{
+    font-size: 0.75rem;
+    margin-top: 1em;
+  }
 `;
 
 const Header = ({ logoAnimDone, setLogoAnimDone }) => {
@@ -17,6 +22,8 @@ const Header = ({ logoAnimDone, setLogoAnimDone }) => {
       <Logo setLogoAnimDone={setLogoAnimDone} />
       <TagLine logoAnimDone={logoAnimDone}>
         Get all the info about all your fav characters!
+        <br/>
+        <span>The table is horizontally scrollable</span>
       </TagLine>
     </>
   );
